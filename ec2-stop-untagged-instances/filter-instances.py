138737caf2fd515e0a2bd8ff02389b3b01d2ad12
Jerome Fontanel <jerome.fontanel@puppet.com>
2021-07-22 12:38:50 +0200

    dev v1.0

--- ec2-stop-untagged-instances/filter-instances.py
+++ ec2-stop-untagged-instances/filter-instances.py
@@ -3,9 +3,10 @@
 # File: filter-instances.py 
 # Description: This is an example script that you can author or modify that retrieves 
 #              a list of instances from the Relay Interface (in the form of parameters)
-#              and filters the instances that are untagged. It then sets the output
-#              variable `instanceIDs` to the list of instances that are untagged. 
+#              and filters the instances that are the tag 'created by' 'Deimos'. It then sets the output
+#              variable `instanceIDs` to the list of instances that have this tag. 
 # Inputs:
+#   - created_by - tag indicating the owner of the instance
 #   - instances - List of instances to evaluate 
 # Outputs:
 #   - instanceIDs - list of instance IDs to stop in the next step
@@ -14,23 +15,26 @@ from relay_sdk import Interface, Dynamic as D
 
 relay = Interface()
 
+# Tag names (user-configurable)
+CREATEDBY = relay.get(D.CreatedBy)
+
 to_stop = []
 to_keep = []
 
 instances = filter(lambda i: i['State']['Name'] == 'running', relay.get(D.instances))
 for instance in instances:
     try:
-        if instance['Tags'] is None: 
+        if instance['Tags'] == CREATEDBY: 
             to_stop.append(instance['InstanceId'])
         else:
             to_keep.append(instance['InstanceId'])
     except Exception as e:
             print('\nEC2 instance {0} not considered for termination because of a processing error: {1}'.format(instance['InstanceId'], e))
 
-print('\nFound {} instances (with tags) to keep:'.format(len(to_keep)))
+print('\nFound {0} instances without the tag createdby = {1} ) to keep:'.format(len(to_keep)), CREATEDBY)
 print(*[instance_id for instance_id in to_keep], sep = "\n") 
 
-print('\nFound {} instances without tags to stop:'.format(len(to_stop)))
+print('\nFound {0} instances with the tag created_by = {1} to stop:'.format(len(to_stop)), CREATEDBY)
 print(*[instance_id for instance_id in to_stop], sep = "\n") 
 
 relay.outputs.set('instanceIDs', to_stop)
