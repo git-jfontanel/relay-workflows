2014bcadb65a36b7fab5e2a6f8ddb00e45d18cbf
Jerome Fontanel <jerome.fontanel@puppet.com>
2021-08-02 20:16:57 +0200

    modif

--- ec2-stop-untagged-instances/filter-instances.py
+++ ec2-stop-untagged-instances/filter-instances.py
@@ -16,33 +16,33 @@ from relay_sdk import Interface, Dynamic as D
 relay = Interface()
 
 # Tag names (user-configurable)
-CREATEDBY = relay.get(D.CreatedBy)
-TAGNAME = relay.get(D.TagName)
+TAGNAME = relay.get(D.Tag_Name1)
+TAGVALUE = relay.get(D.Tag_Value1)
 
 to_stop = []
 to_keep = []
 to_keep_notag = []
 
-instances = filter(lambda i: i['State']['Name'] == 'running', relay.get(D.instances))
 for instance in instances:
     try:
         if instance['Tags']is None: 
             to_keep_notag.append(instance['InstanceId'])
         for tag in instance['Tags']:
-            if tag['Key'] == TAGNAME and tag['Value'] == CREATEDBY:
-                to_stop.append(instance['InstanceId'])
-        else:
-            to_keep.append(instance['InstanceId'])
+            if tag['Key'] == TAGNAME  
+                if tag['Value'] == TAGVALUE:
+                    to_stop.append(instance['InstanceId'])
+            else:
+                to_keep.append(instance['InstanceId'])
     except Exception as e:
             print('\nEC2 instance {0} not considered for termination because of a processing error: {1}'.format(instance['InstanceId'], e))
 
-print('\n\nFound {0} instances with no tags, have a look on these instances:'.format(len(to_keep_notag)))
+print('\n\nFound {} instances with no tags, check these instances:'.format(len(to_keep_notag)))
 print(*[instance_id for instance_id in to_keep_notag], sep = "\n") 
 
-print('\nFound {0} instances without the tag createdby = ' + CREATEDBY + ' to keep:'.format(len(to_keep)))
+print('\nFound {} instances without the tag ' + TAGNAME + '  = ' + TAGVALUE + ' to keep:'.format(len(to_keep)))
 print(*[instance_id for instance_id in to_keep], sep = "\n") 
 
-print('\nFound {0} instances with the tag created_by = ' + CREATEDBY + ' to stop:'.format(len(to_stop)))
+print('\nFound {} instances with the tag ' + TAGNAME + ' = ' + TAGVALUE + ' to stop:'.format(len(to_stop)))
 print(*[instance_id for instance_id in to_stop], sep = "\n") 
 
 relay.outputs.set('instanceIDs', to_stop)
